0e4fc32850dbc3ac34234dc163c819560ad47a60
k22rs100 <k22rs100@st.kyusan-u.ac.jp>
2023-12-20 22:43:13 +0900

    Project's Initial Commit

---
+++ www/keys.js
@@ -0,0 +1,4 @@
+// This is a JavaScript file
+
+let appKey="d853f596d4487e64d9def2174ca93c51352d5e0e3a6ddbb78579fc9cf12294ab";
+let clientKey="64f438f8c0504847273d3b7c9b6eb49f18b3854a3b1dde453ddc37eef31c8480";
